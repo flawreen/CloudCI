01460b159bf68ff6c0f4573ab42559ab5b47a1d9
flawreen <craciun.florin1512@gmail.com>
2025-04-25 22:56:06 +0300

    added 2 tests for /best-by-location

--- routes/airbnb.js
+++ routes/airbnb.js
@@ -10,6 +10,9 @@ router.get('/ping', (req, res) => {
     res.send('Pong from Airbnb router!');
 });
 
+/**
+ * Finds a booking by id
+ */
 router.get('/id/:id', async (req, res) => {
     const db = await connectToDb();
     const collection = await db.collection(process.env.COLLECTION_NAME);
@@ -20,53 +23,62 @@ router.get('/id/:id', async (req, res) => {
     } else res.send(qry);
 });
 
+/**
+ *  Finds at most 10 bookings in the requested city having the review scores average above:
+ -> 6 to be recommended for a friends trip
+ -> 9 to be recommended for a family trip
+ */
 router.get('/best-by-location/:location', async (req, res) => {
     const db = await connectToDb();
     const collection = await db.collection(process.env.COLLECTION_NAME);
     // convert %20 into spaces
     req.params.location.replaceAll('%20', ' ');
+
     let top10 = [];
-    for (let i = 1; ; ++i) {
+    for (let i = 1; top10.length < 10; ++i) {
         let bookings = await collection
             .find({'address.market': req.params.location})
             .project({review_scores: 1})
-            .limit(5)
-            .skip((i - 1) * 5).toArray();
+            .limit(10)
+            .skip((i - 1) * 10).toArray();
+
+        if (bookings.length < 1) {
+            res.sendStatus(404);
+            return;
+        }
 
         for (const booking of bookings) {
-            // calculating average review score
-            let scores_no = Object.keys(booking.review_scores).length - 1;
-            let scores_sum = 0;
-            let scores_array = Object.values(booking.review_scores);
-            for (let k = 0; k < scores_no; ++k) {
-                scores_sum += scores_array[k];
-            }
-            const avg_score = scores_sum / scores_no;
+            const avg_score = Object
+                .values(booking.review_scores)
+                .slice(0, -1)
+                .reduce((sum, score) => sum + score, 0) / (Object.keys(booking.review_scores).length - 1);
 
             // calculating the recommendation grade
             let recommendation;
             if (avg_score > 6) {
-                recommendation = "Friends trip";
-                if (avg_score > 9) {
-                    recommendation = "Family trip";
-                }
+                recommendation = avg_score > 9 ? "Family trip" : "Friends trip";
             } else continue;
 
-            top10.push({
+
+            if (top10.length >= 10) {
+                break;
+            } else top10.push({
                 _id: booking._id,
                 location: req.params.location,
                 recommendation: recommendation,
                 avg_score: avg_score.toFixed(2)
             });
         }
-        if (top10.length >= 10) {
-            break;
-        }
     }
 
-    res.send(top10);
+    if (top10.length < 1) {
+        res.sendStatus(404);
+    } else res.send(top10);
 });
 
+/**
+ * Finds at most 10 bookings with the requested facility
+ */
 router.get('/with-facility/:facility', async (req, res) => {
     const db = await connectToDb();
     const collection = await db.collection(process.env.COLLECTION_NAME);
--- tests/airbnb.test.js
+++ tests/airbnb.test.js
@@ -9,9 +9,7 @@ describe('GET /airbnb/id', () => {
         expect(res.status).toBe(200);
         expect(res.body._id).toBe('1003530');
     });
-});
 
-describe('GET /airbnb/id', () => {
     it('return 404', async () => {
         const res = await request(app).get('/airbnb/id/no_id');
         expect(res.status).toBe(404);
@@ -22,21 +20,44 @@ describe('GET /airbnb/with-facility', () => {
     it('return 200 and find at most 10 bookings with the correct facility', async () => {
         const res = await request(app).get('/airbnb/with-facility/Washer');
         expect(res.status).toBe(200);
+
         const bookings = res.body;
+
+        expect(bookings.length).toBeLessThanOrEqual(10);
+
         for (const booking of bookings) {
             expect(booking.amenities).toContain('Washer');
         }
-        expect(bookings.length).toBeLessThanOrEqual(10);
     });
-});
-
-describe('GET /airbnb/with-facility', () => {
     it('return 404', async () => {
         const res = await request(app).get('/airbnb/with-facility/Golf land');
         expect(res.status).toBe(404);
     });
 });
 
+describe('GET /airbnb/best-by-location', () => {
+    const timeout = 5000;
+    it('return 200 and find at most 10 bookings in the requested city ranked by review scores average: above 6 for friends trip, above 9 for family trip', async () => {
+        const res = await request(app).get('/airbnb/best-by-location/New York');
+        expect(res.status).toBe(200);
+        const bookings = res.body;
+        expect(bookings.length).toBeLessThanOrEqual(10);
+
+        for (const booking of bookings) {
+            expect(booking.location).toBe('New York');
+            expect(Number.parseFloat(booking.avg_score)).toBeGreaterThanOrEqual(6);
+            if (Number.parseFloat(booking.avg_score) > 9) {
+                expect(booking.recommendation).toBe('Family trip');
+            } else {
+                expect(booking.recommendation).toBe('Friends trip');
+            }
+        }
+    }, timeout);
+    it('return 404', async () => {
+        const res = await request(app).get('/airbnb/best-by-location/Narnia');
+        expect(res.status).toBe(404);
+    }, timeout);
+});
 
 afterAll(async () => {
     await closeDbConnection();
